2b98dc2cd0eeeb5969606344da76123167104c9f
DavideCavallotto <davidecavallotto92@gmail.com>
2023-11-30 16:43:02 +0100

    ADD database

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Base</title>
+</head>
+<body>
+    <img src="./database_parco_auto.png" alt="">
+</body>
+</html>
